eee6db72bc827e3215e8b248dcc1942be66b7043
utolee90 <itdatabasewiki@gmail.com>
2024-08-28 21:09:41 +0900

    Piformer 소스 추가

--- models/Piformer.py
+++ models/Piformer.py
@@ -5,6 +5,7 @@ from layers.Transformer_EncDec import Encoder, EncoderLayer
 from layers.SelfAttention_Family import FullAttention, AttentionLayer
 from layers.Embed import DataEmbedding_inverted
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
 
 class Model(nn.Module):
@@ -18,8 +19,25 @@ class Model(nn.Module):
         self.seq_len = configs.seq_len
         self.pred_len = configs.pred_len
         self.output_attention = configs.output_attention
+        self.d_model = configs.d_model
+
+        self.joint_var = configs.joint_var # 패칭후 변수 상관관계 여부
+
+        # get parameters
+        self.enc_in = configs.enc_in
+        self.period_len = configs.period_len
+
+        self.seg_num_x = self.seq_len // self.period_len
+        self.seg_num_y = self.pred_len // self.period_len
+
+        self.conv1d = nn.Conv1d(in_channels=1, out_channels=1, kernel_size=1 + 2 * (self.period_len // 2),
+                                stride=1, padding=self.period_len // 2, padding_mode="zeros", bias=False)
+
+        self.linear = nn.Linear(self.seg_num_x, self.seg_num_y, bias=False)
+
+
         # Embedding
-        self.enc_embedding = DataEmbedding_inverted(configs.seq_len, configs.d_model, configs.embed, configs.freq,
+        self.enc_embedding = DataEmbedding_inverted(self.seg_num_x, configs.d_model, configs.embed, configs.freq,
                                                     configs.dropout)
         # Encoder
         self.encoder = Encoder(
@@ -37,8 +55,8 @@ class Model(nn.Module):
             norm_layer=torch.nn.LayerNorm(configs.d_model)
         )
         # Decoder
-        if self.task_name == 'long_term_forecast' or self.task_name == 'short_term_forecast':
-            self.projection = nn.Linear(configs.d_model, configs.pred_len, bias=True)
+        if self.task_name in ['long_term_forecast', 'short_term_forecast', 'long_term_forecast_partial']:
+            self.projection = nn.Linear(configs.d_model, self.seg_num_y, bias=True)
         if self.task_name == 'imputation':
             self.projection = nn.Linear(configs.d_model, configs.seq_len, bias=True)
         if self.task_name == 'anomaly_detection':
@@ -47,6 +65,8 @@ class Model(nn.Module):
             self.act = F.gelu
             self.dropout = nn.Dropout(configs.dropout)
             self.projection = nn.Linear(configs.d_model * configs.enc_in, configs.num_class)
+        
+
 
     def forecast(self, x_enc, x_mark_enc, x_dec, x_mark_dec):
         # Normalization from Non-stationary Transformer
@@ -57,11 +77,29 @@ class Model(nn.Module):
 
         _, _, N = x_enc.shape
 
+        # linear_coef
+        x_enc = x_enc.permute(0,2,1)
+        # 1D convolution aggregation
+        # x_enc = self.conv1d(x_enc.reshape(-1, 1, self.seq_len)).reshape(-1, self.enc_in, self.seq_len) + x_enc
+        x_enc = x_enc.permute(0,2,1)
+
+        # downsampling: b,c,s -> b,c,n,w -> b,cw,n
+        x_enc = x_enc.reshape(-1, self.enc_in,  self.seg_num_x, self.period_len)
+        x_enc = x_enc.permute(0, 1, 3, 2).reshape(-1, self.enc_in*self.period_len, self.seg_num_x).permute(0,2,1)
+        x_mark_enc = x_mark_enc[:, :self.seg_num_x, :]
+        # print(x_enc.shape, x_mark_enc.shape)
+        # x_mark_enc = x_mark_enc.reshape(-1, self.period_len*self.enc_in, self.seg_num_x).permute(0, 2, 1)
+
         # Embedding
-        enc_out = self.enc_embedding(x_enc, x_mark_enc)
+        enc_out = self.enc_embedding(x_enc, None)
         enc_out, attns = self.encoder(enc_out, attn_mask=None)
 
-        dec_out = self.projection(enc_out).permute(0, 2, 1)[:, :, :N]
+        # dec_out -> Projection -> composition
+        dec_out = self.projection(enc_out) # shape bc, w, m
+        dec_out = dec_out.reshape(-1, self.enc_in, self.period_len, self.seg_num_y )
+        dec_out = dec_out.permute(0, 1, 3, 2).reshape(-1, self.enc_in, self.pred_len).permute(0,2,1)
+
+
         # De-Normalization from Non-stationary Transformer
         dec_out = dec_out * (stdev[:, 0, :].unsqueeze(1).repeat(1, self.pred_len, 1))
         dec_out = dec_out + (means[:, 0, :].unsqueeze(1).repeat(1, self.pred_len, 1))
@@ -116,6 +154,28 @@ class Model(nn.Module):
         output = output.reshape(output.shape[0], -1)  # (batch_size, c_in * d_model)
         output = self.projection(output)  # (batch_size, num_classes)
         return output
+    
+    # 부드럽게 Tensor 연결하는 함수 정의
+    def smoothing(X):
+        # X is a 4D tensor of shape (a, b, c, d)
+        # weights is a 1D tensor of shape (2,)
+        
+        a, b, c, d = X.shape
+        # 스택하고 리쉐이프하는 과정
+        X_stacked = X.permute(1, 2, 0, 3).reshape(b, c, a * d)
+        
+        # 2개씩 짝을 지어 처리
+        for j in range(a-1):  
+            ct_point = d*(j+1)
+
+            ct_before = X_stacked[:, :, ct_point-1]
+            ct_after = X_stacked[:, :, ct_point]
+            for k in range(d//2):
+                X_stacked[:, :, k + ct_point - d//2] = (d-k)/(d+1) * X_stacked[:, :, k + ct_point - d//2] + (k+1)/(d+1) * ct_after
+            for k in range(d//2):
+                X_stacked[:, :, k + ct_point ] = (d//2 + k +1)/(d+1) * X_stacked[:, :, k + ct_point ] + (d//2 - k)/(d+1) * ct_before
+        
+        return X_stacked
 
     def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask=None):
         if self.task_name in ['long_term_forecast', 'short_term_forecast', 'long_term_forecast_partial']:
