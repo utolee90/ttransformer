e66a74caf6778db2b7b2b84d5559606e73feafd9
utolee90 <itdatabasewiki@gmail.com>
2024-08-29 22:35:48 +0900

    Load Model 파일 수정

---
+++ scripts/long_term_forecast/Traffic_script/Piformer_compare.sh
@@ -0,0 +1,54 @@
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=Piformer
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_336_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --des 'Exp' \
+  --d_model 16 \
+  --d_ff 16 \
+  --batch_size 4 \
+  --learning_rate 0.0001 \
+  --itr 3 \
+  --period_len 24
+
+  python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_336_720 \
+  --model iTransformer \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --des 'Exp' \
+  --d_model 256 \
+  --d_ff 256 \
+  --batch_size 4 \
+  --learning_rate 0.0001 \
+  --itr 3 \
