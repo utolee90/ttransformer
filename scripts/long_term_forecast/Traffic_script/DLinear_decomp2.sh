74aacabcf47182b04c8f2cf7467e9d96da18c82e
utolee90 <itdatabasewiki@gmail.com>
2024-09-10 19:36:31 +0900

    Smooth_move 파일 추가로 DLinear, PITS의 seasonal/trend 파트 속성 분석

---
+++ scripts/long_term_forecast/Traffic_script/DLinear_decomp2.sh
@@ -0,0 +1,29 @@
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=DLinear_decomp
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/traffic/ \
+  --data_path traffic.csv \
+  --model_id traffic_96_96_D_m97 \
+  --model $model_name \
+  --data custom \
+  --features MS \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 1 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --des 'Exp' \
+  --d_model 512 \
+  --d_ff 512 \
+  --batch_size 8 \
+  --learning_rate 0.0005 \
+  --moving_avg 97 \
+  --itr 1
